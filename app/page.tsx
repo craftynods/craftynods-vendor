37efca47d702d4986cbd04c15b06985ff24ec232
Ujjawal <ujjawal.craftynods@2gmail.com>
2025-04-10 21:15:24 +0530

    vendor dashbaord layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-"use client"
-import Navbar from '@/components/navbar';
-import { getVendorCookiesandFetchVendor } from '@/lib/database/actions/vendor/vendor.actions';
-import React, { useEffect, useState } from 'react'
+"use client";
+import Navbar from "@/components/navbar";
+import { getVendorCookiesandFetchVendor } from "@/lib/database/actions/vendor/vendor.actions";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
   const [vendor, setVendor] = useState<any>(null);
@@ -9,30 +9,31 @@ const HomePage = () => {
     try {
       const fetchVendorDetails = async () => {
         try {
-          await getVendorCookiesandFetchVendor().then((res)=>{
-            if(res?.success) {
-              setVendor(res?.vendor);``
+          await getVendorCookiesandFetchVendor().then((res) => {
+            if (res?.success) {
+              setVendor(res?.vendor);
             }
-
-          }) 
+          });
         } catch (error: any) {
           console.log(error);
         }
-      }
+      };
+      fetchVendorDetails();
     } catch (error: any) {
       console.log(error);
     }
-  }, [])
+  }, []);
   return (
     <div>
-       <Navbar/>
-      {!vendor && !vendor?.verified && (
-      <div className='flex item-center justify-center bg-red-100'>
-      <p><b>Note:</b> You are not yet verified by an admin, so you don't hae access to the dashboard!!!</p>
-      </div>
-    )}
+      <Navbar />
+      {vendor && !vendor?.verified && (
+        <div className="flex items-center justify-center bg-red-100">
+          <b>Note:</b> You're not yet verified by an admin, so you do'nt have
+          access to Dashboard!
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
-"use client"
-import {Button, Group} from "@mantine/core";
+"use client";
+import { Button, Group } from "@mantine/core";
 import { useRouter } from "next/navigation";
-// import {logout}
+import { logout } from "@/lib/database/actions/vendor/auth/logout";
 import { useEffect, useState } from "react";
 import { getVendorCookiesandFetchVendor } from "@/lib/database/actions/vendor/vendor.actions";
 
@@ -9,57 +9,60 @@ import React from "react";
 import Logo from "./Logo";
 
 const Navbar = () => {
-    const router = useRouter();
   const [vendor, setVendor] = useState<any>(null);
-    useEffect(() => {
-      try {
-        const fetchVendorDetails = async () => {
-          try {
-            await getVendorCookiesandFetchVendor().then((res)=>{
-              if(res?.success) {
-                setVendor(res?.vendor);
-              }
-  
-            }) 
-          } catch (error: any) {
-            console.log(error);
-          }
+  const router = useRouter();
+  useEffect(() => {
+    try {
+      const fetchVendorDetails = async () => {
+        try {
+          await getVendorCookiesandFetchVendor().then((res) => {
+            if (res?.success) {
+              setVendor(res?.vendor);
+            }
+          });
+        } catch (error: any) {
+          console.log(error);
         }
-      } catch (error: any) {
-        console.log(error);
-      }
-    }, [])
+      };
+      fetchVendorDetails();
+    } catch (error: any) {
+      console.log(error);
+    }
+  }, []);
+  return (
+    <header className="p-[1rem] border-b-[1px] border-b-[#eaeaea]">
+      <nav className="flex justify-between items-center">
+        <Logo />
+        <Group>
+          {vendor && vendor.name ? (
+            <div className="flex gap-[10px]">
+              <Button
+                variant="outline"
+                onClick={() => router.push("/vendor/dashboard")}
+              >
+                Vendor Dashboard
+              </Button>
+              <Button
+                onClick={() => {
+                  logout();
+                  router.refresh();
+                }}
+              >
+                Logout
+              </Button>
+            </div>
+          ) : (
+            <div className="flex gap-[10px]">
+              <Button variant="outline" onClick={() => router.push("/signin")}>
+                Sign in
+              </Button>
+              <Button onClick={() => router.push("/signup")}>Sign Up</Button>
+            </div>
+          )}
+        </Group>
+      </nav>
+    </header>
+  );
+};
 
-    return (
-        <header className=" p-[1rem] border-b-[1px] border-b-[#eaeaea]">
-         <nav className="flex justify-between items-center">
-            <Logo/>
-            <Group>
-                {vendor && vendor.name?(
-                    <div className="flex gap-[10px]">
-                        <Button variant="outline" onClick={()=>router.push("/vendor/dashboard")}>
-                            Vendor Dashboard
-                        </Button>
-                        <Button  onClick={()=>{
-                            //logout();
-                            router.refresh();
-                        }}>
-                            Vendor Dashboard
-                        </Button>
-                    </div>
-                ):(
-                    <div className="flex gap-[10px]">
-                        <Button variant="outline" onClick={()=>router.push("/signin")}>
-                            Sign in
-                        </Button>
-                        <Button onClick={()=>router.push("/signup")}>
-                            Sign up
-                        </Button>
-                    </div>
-                )}
-            </Group>
-         </nav>
-        </header>
-    )
-}
 export default Navbar;
